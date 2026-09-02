60e61beecf2be9c1d8c0c41d605111a82981fe7b
John Hsieh <hsiehjun@google.com>
2026-09-02 20:19:48 +0000

    fix(auth): import timedelta in auth.py for user 24h invite pass generation

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 import base64
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 import hashlib
 import json
 import logging
